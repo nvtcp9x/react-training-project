1b4185dcd20f60ac5896b36642db193134394ffa
bui.duc.cuong <bui.duc.cuong@framgia.com>
2018-07-03 11:50:02 +0700

    Simple init webpack & config to transpiling react code

---
+++ STEP.md
@@ -0,0 +1,20 @@
+# Installation
+  * Required Node.js
+  * Latest webpack v4.14.0
+  * Install via NPM
+# Getting Started
+## Basic Setup
+  - npm init -> pakage.json
+  - npm install webpack & webpack-cli
+    *liC stand for command Line Interface*
+    *CLI to configure and tnteract with our build.*
+  - run webpack-cli init
+  - edit webpack.config.js
+  - setting up HtmlWebpackPlugin
+    *Create Html file and inject bundle js file*
+  - install babel to transpiling javascript ES2015+ to ES5
+  - create .babelrc
+## Setup React
+  - install react react-dom babel-preset-react
+    *babel-preset-react to compile react code -> js*
+  - edit .babelrc for react
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(scss|css)$/,
+
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJSPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'My Title',
+      template: './src/index.html'
+    }),
+  ],
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  mode: 'development'
+};
